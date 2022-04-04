43317023b347536d9e63107c7ad8b50025b08ea0
Thiago Bernardi <copocaneta@gmail.com>
2022-04-03 23:33:11 -0300

    test: ensured popover responds to hover

--- 02-sundaes-on-demand/src/pages/summary/SummaryForm.jsx
+++ 02-sundaes-on-demand/src/pages/summary/SummaryForm.jsx
@@ -1,15 +1,35 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Popover, OverlayTrigger } from 'react-bootstrap'
+
+//   const Example = () => (
+//     <OverlayTrigger trigger="click" placement="right" overlay={popover}>
+//       <Button variant="success">Click me to see</Button>
+//     </OverlayTrigger>
+//   );
+
+//   render(<Example />);
 
 const SummaryForm = () => {
   const [buttonDisable, setButtonDisable] = useState(false)
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  )
+
   return (
     <>
       <Form>
         <Form.Group>
           <Form.Group>
             <Form.Check id="terms-checkbox" type="checkbox" onChange={(e) => setButtonDisable(e.target.checked)} />
-            <Form.Label htmlFor="terms-checkbox">I agree to Terms and Conditions</Form.Label>
+            <Form.Label htmlFor="terms-checkbox">
+              I agree to
+              <OverlayTrigger placement="right" overlay={popover}>
+                <Button variant="success">Terms and Conditions</Button>
+              </OverlayTrigger>
+            </Form.Label>
           </Form.Group>
           <Button variant="primary" type="Submit" disabled={!buttonDisable}>
             Confirm Order
--- 02-sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
+++ 02-sundaes-on-demand/src/pages/summary/test/SummaryForm.test.jsx
@@ -1,5 +1,6 @@
-import { render, screen, fireEvent } from '@testing-library/react'
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react'
 import SummaryForm from '../SummaryForm'
+import userEvent from '@testing-library/user-event'
 
 test('ensure that checkbox is unchecked by default', () => {
   render(<SummaryForm />)
@@ -17,8 +18,32 @@ test('ensure that checkbox enables and disables button', () => {
   render(<SummaryForm />)
   const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' })
   const submitButton = screen.getByRole('button', { name: 'Confirm Order' })
-  fireEvent.click(checkbox)
+  userEvent.click(checkbox)
   expect(submitButton).toBeEnabled()
-  fireEvent.click(checkbox)
+  userEvent.click(checkbox)
   expect(submitButton).toBeDisabled()
+})
+
+test('popover responds to hover', async () => {
+  render(<SummaryForm />)
+  // popover starts hidden
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i)
+  expect(nullPopover).not.toBeInTheDocument()
+  // popover appears up mouseouver of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i)
+  userEvent.hover(termsAndConditions)
+  const popover = screen.getByText(/no ice cream will actually be delivered/i)
+  //
+  // no need to for the assesrtion below
+  expect(popover).toBeInTheDocument()
+  // because if screen.getByText doesn't get any results back, it's going to 'throw' and then fail the test
+  // but it's best practice to include it anyway, because it makes the test more readable
+  //
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions)
+  let nullPopoverAgain
+  await waitForElementToBeRemoved(
+    () => (nullPopoverAgain = screen.queryByText(/no ice cream will actually be delivered/i))
+  )
+  expect(nullPopoverAgain).not.toBeInTheDocument()
 })
