90e380bc3cae35b510d3af7ee13cda84cd99f7fc
Thiago Bernardi <copocaneta@gmail.com>
2022-04-07 08:46:35 -0300

    test: ensuring our tests handle errors and mocking errors with server.resetHandlers() from msw in the test

--- 02-sundaes-on-demand/src/pages/entry/Options.jsx
+++ 02-sundaes-on-demand/src/pages/entry/Options.jsx
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { Row } from 'react-bootstrap'
-import axios from 'axios'
-import ToppingOption from './ToppingOption'
-import ScoopOption from './ScoopOption'
+import React, { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import axios from 'axios';
+import ToppingOption from './ToppingOption';
+import ScoopOption from './ScoopOption';
+import AlertBanner from '../common/AlertBanner';
 
 const Options = ({ optionType }) => {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
   // optionType is 'scoops' or 'toppings'
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => {
-        // TODO: habndle error response
-      })
-  }, [optionType])
+      .catch((error) => setError(true));
+  }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   // TODO: Replace `null` with ToppingOption when it's available
-  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
-  const optionItems = items.map((item) => <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />)
+  const optionItems = items.map((item) => (
+    <ItemComponent key={item.name} name={item.name} imagePath={item.imagePath} />
+  ));
 
-  return <Row>{optionItems}</Row>
-}
-export default Options
+  return <Row>{optionItems}</Row>;
+};
+export default Options;
