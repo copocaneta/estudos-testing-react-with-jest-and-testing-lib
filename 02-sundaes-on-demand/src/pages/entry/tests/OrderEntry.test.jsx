ecd1f13b4189732044994d42e8e190bf21b07a78
Thiago Bernardi <copocaneta@gmail.com>
2022-04-21 19:40:33 -0300

    refactor: added jest function mock for one test thhat didnt require the funciton but had typescript complaining it was missing

--- 02-sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
+++ 02-sundaes-on-demand/src/pages/entry/tests/OrderEntry.test.jsx
@@ -9,7 +9,7 @@ test('handles errors for scoops and toppings routes', async () => {
     rest.get('http://localhost:3030/scoops', (req, res, ctx) => res(ctx.status(500))),
     rest.get('http://localhost:3030/toppings', (req, res, ctx) => res(ctx.status(500)))
   );
-  render(<OrderEntry />);
+  render(<OrderEntry setOrderPhase={jest.fn()} />);
   await waitFor(async () => {
     const alerts = await screen.findAllByRole('alert');
     expect(alerts).toHaveLength(2);
